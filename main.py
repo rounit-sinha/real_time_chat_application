e1e5a3ccaedcb540bf87f55ca7cfc1a54a2b46a8
Rounit Ranjan <130220877+rounit-sinha@users.noreply.github.com>
2024-02-26 19:55:38 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+
+import requests
+
+PRIVATE_KEY = "b311a1f3-8e5e-4417-9840-b07d2d6459e5"
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class User(BaseModel):
+    username: str
+
+@app.post('/authenticate')
+async def authenticate(user: User):
+    response = requests.put('https://api.chatengine.io/users/',
+        data={
+            "username": user.username,
+            "secret": user.username,
+            "first_name": user.username,
+        },
+        headers={ "Private-Key": PRIVATE_KEY }
+    )
+    return response.json()
